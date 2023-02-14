21c7ca6d12d91be0b6dc66d406e76f0084b250a9
Pál Patrik Zoltán <116691534+aene11e@users.noreply.github.com>
2023-02-14 16:41:14 +0100

    install.sh
    Deb AD Installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+echo -e "Hello, ${whoami} \Udvozollek a Debian Samba AD telepitovarazslojaban."
+echo "Gyozodj meg rola ,hogy elvegezted az elokonfiguracios beallitasokat az alabbi fajlokban\:"
+echo "\/etc\/hostname"
+echo "\/etc\/hosts"
+echo "\/etc\/network\/interfaces"
+echo "Telepites\:\[Y,n\]
+valasz=""
+read valasz
+#if [valasz -eq "Y"
