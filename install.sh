cb92ee0ed14e6b07306c9cdc5c9a6c37744e0161
Pál Patrik Zoltán <116691534+aene11e@users.noreply.github.com>
2023-02-15 18:57:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ echo ">  /etc/hostname"
 echo ">  /etc/hosts"
 echo ">  /etc/network/interfaces"
 echo ">  /etc/resolv.conf"
-read -p "Telepites:[Y,n]" valasz
+read -p "Telepites:[Y,n] " valasz
 if [ $valasz = "Y" ]
 then
-  read -p "Rendszerfrissites [ENTER]"
+  read -p "Rendszerfrissites [ENTER] "
   apt-get update -o Acquire::ForceIPv4=True && apt-get dist-upgrade -o Acquire::ForceIPv4=True -y
-  read -p "Telepites [ENTER]"
+  read -p "Telepites [ENTER] "
   apt-get install -y tree mc isc-dhcp-server openssh-server apache2 htop ufw unattended-upgrades cabextract libmspack0  rkhunter ntp samba smbclient winbind krb5.config krb5.user
   wget "http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb"
   dpkg -i ttf-mscorefonts-installer_3.6_all.deb 
