5fe93f10bfa65232ab1701de5868a4e397427e96
Pál Patrik Zoltán <116691534+aene11e@users.noreply.github.com>
2023-02-15 14:16:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ then
   read -p "Rendszerfrissites [ENTER]"
   apt-get update -o Acquire::ForceIPv4=True && apt-get dist-upgrade -o Acquire::ForceIPv4=True -y
   read -p "Telepites [ENTER]"
-  apt-get install tree mc isc-dhcp-server openssh-server apache2 htop ufw unattended-upgrades update-notifier-common rkhunter ntp samba smbclient winbind krb5.config krb5.user -y
+  apt-get install tree mc isc-dhcp-server openssh-server apache2 htop ufw unattended-upgrades apt-get install cabextract libmspack0  rkhunter ntp samba smbclient winbind krb5.config krb5.user -y
+  wget "http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb"
+  dpkg -i ttf-mscorefonts-installer_3.6_all.deb 
 fi
 rm /etc/samba/smb.conf
 rm /etc/krb5.conf
