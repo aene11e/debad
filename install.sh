930c8308f021d09f5f340184dbb1cab816891c63
Pál Patrik Zoltán <116691534+aene11e@users.noreply.github.com>
2023-02-16 08:47:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 #!bin/bash
 clear
+tput setaf 1 bold
 echo "Udvozollek a Debian Samba AD telepitovarazslojaban."
 echo "Gyozodj meg rola ,hogy elvegezted az elokonfiguracios beallitasokat az alabbi fajlokban:"
 echo ">  /etc/hostname"
 echo ">  /etc/hosts"
 echo ">  /etc/network/interfaces"
 echo ">  /etc/resolv.conf"
-read -p "Telepites:[Y,n] " valasz
+tput setaf 2 bold && read -p "Telepites:[Y,n] " valasz  && tput sgr0
 if [ $valasz = "Y" ]
 then
-  read -p "Rendszerfrissites [ENTER] "
+  tput setaf 2 bold && read -p "Rendszerfrissites [ENTER] " && tput sgr0
   apt-get update -o Acquire::ForceIPv4=True && apt-get dist-upgrade -o Acquire::ForceIPv4=True -y
-  read -p "Telepites [ENTER] "
+  tput setaf 2 bold && read -p "Telepites [ENTER] " && tput sgr0
   apt-get install -y tree mc isc-dhcp-server openssh-server apache2 htop ufw unattended-upgrades cabextract libmspack0  rkhunter ntp samba smbclient winbind krb5.config krb5.user
   wget "http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb"
   dpkg -i ttf-mscorefonts-installer_3.6_all.deb 
-  read -p "Az alap Samba és Kerberos conf fájl törlése [ENTER] "
+  tput setaf 2 bold && read -p "Az alap Samba és Kerberos conf fájl törlése [ENTER] " && tput sgr0
   rm /etc/samba/smb.conf
   rm /etc/krb5.conf
-  read -p "Samba-tool Domain beállítások [ENTER] "
+  tput setaf 2 bold && read -p "Samba-tool Domain beállítások [ENTER] " && tput sgr0
   samba-tool domain provision --use-rfc2307 --realm=docnetic.net --domain=doc --server-role=dc --adminpass=S3cr3tvizsga!!
   cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
   systemctl stop smbd nmbd winbind
