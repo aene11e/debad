3ba012593997aabf87105b632ac51f367954c68c
Pál Patrik Zoltán <116691534+aene11e@users.noreply.github.com>
2023-02-16 12:01:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,10 +32,7 @@ then
   tput bold && tput setaf 2 && read -p "Reverse Zone letrehozasa [ENTER] " && tput sgr0
   samba-tool dns zonecreate 10.0.0.66 66.0.0.10.in-addr.arpa -U administrator
   tput bold && tput setaf 1 && echo "kdc = mydc.mydomain.com:88 KDC server megjelolese krb5.confban!" && tput sgr0
-  ufw allow 53 
-  ufw allow 88
-  ufw allow 80 
-  ufw allow 443 
+  ufw allow 53 && ufw allow 88 && ufw allow 80 && ufw allow 443 
 else
   clear
   tput bold && tput setaf 2 && read -p "Teszteles (NTP) [ENTER] " && tput sgr0
