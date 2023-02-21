7e5a259254ebceca7f20360a79f73811a6aca0f9
Pál Patrik Zoltán <116691534+aene11e@users.noreply.github.com>
2023-02-21 18:43:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ then
   ufw allow 67/udp && ufw allow 68/udp && ufw allow DNS && ufw allow WWW Full && ufw allow WWW Cache
   ufw allow Kerberos Admin && ufw allow Kerberos Password && ufw allow Kerberos KDC && ufw allow Kerberos Full && ufw allow LDAP
   ufw allow LDAPS && ufw allow OpenSSH
-  ufw allow from 10.0.0.64/29 && ufw status verbose
+  ufw allow to any from 10.0.0.64/29 && ufw status verbose
   tput bold && tput setaf 2 && read -p "Rendszerfrissites [ENTER] " && tput sgr0
   apt-get update -o Acquire::ForceIPv4=True && apt-get dist-upgrade -o Acquire::ForceIPv4=True -y
   tput bold && tput setaf 2 && read -p "Telepites [ENTER] " && tput sgr0
